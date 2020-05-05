f2f264b12af008c645c1bb524f83ffda7652b8a6
MHR-2477380 <ru.ru.ru.247738@gmail.com>
2020-05-05 16:56:21 +0000

    feature [create] postsコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  get "/" => "home#top"
-  get "about" => "home#about"
+  get "/" => "homes#top"
+  get "about" => "homes#about"
+
+  get "posts/index" => "posts#index"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
