ba8efe5f8f7a6223578e9de21590ec824854789f
MHR-2477380 <ru.ru.ru.247738@gmail.com>
2020-05-05 00:06:51 +0000

    develop [create] aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get "top" => "home#top"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "about" => "home#about"
   
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
