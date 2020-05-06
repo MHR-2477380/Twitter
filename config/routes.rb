fe682cabb2bc7adff3f4b80eadec188453e1234c
MHR-2477380 <ru.ru.ru.247738@gmail.com>
2020-05-06 00:14:13 +0000

    feature [create] showページ

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "about" => "homes#about"
 
   get "posts/index" => "posts#index"
+  get "posts/:id" => "posts#show"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
