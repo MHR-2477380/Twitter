649446e780105745840ad5e7450dc61e0faf42d0
MHR-2477380 <ru.ru.ru.247738@gmail.com>
2020-05-17 09:41:11 +0000

    feature [create] いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Rails.application.routes.draw do
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
 
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
